31218aedb4b72124f788a4fe8f0ab0b57a0f7df7
Craig McLeod <mcleod.craig@outlook.com>
2020-06-05 16:20:16 +0100

    Added date of birth into table structure

--- db/pace.sql
+++ db/pace.sql
@@ -9,6 +9,7 @@ CREATE TABLE vets (
 CREATE TABLE animals (
     id SERIAL primary key,
     name VARCHAR(255),
+    date_of_birth VARCHAR(255),
     animal_type VARCHAR(255),
     owner_name VARCHAR(255),
     owner_phone_number VARCHAR(255),
