d3f57f0b17f92f3cc18cb095de605b0304467d84
Craig McLeod <mcleod.craig@outlook.com>
2020-06-06 09:22:33 +0100

    Initial work on homepage view

--- controller.rb
+++ controller.rb
@@ -4,6 +4,10 @@ require_relative('models/animal')
 require_relative('models/vet')
 also_reload('./models/*')
 
+get '/homepage' do
+    erb( :homepage)
+end
+
 get '/animals' do
     @animals = Animal.all
     erb( :index)
