e497480fa34973becd9f6f771884b0a9d10500b8
Craig McLeod <mcleod.craig@outlook.com>
2020-06-06 20:31:22 +0100

    Heavy refactoring to begin use of multiple controllers. Functionality restored for animals

--- controllers/animals_controller.rb
+++ controllers/animals_controller.rb
@@ -1,40 +1,28 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('models/animal')
-require_relative('models/vet')
-also_reload('./models/*')
-
-get '/homepage' do
-    erb( :homepage)
-end
-
-get '/admin' do
-    erb( :admin)
-end
-
-# get '/consultation' do
-#     erb( consulation/consultation)
-# end
+require_relative('../models/animal')
+require_relative('../models/vet')
+also_reload('../models/*')
 
 get '/animals' do #index
     @animals = Animal.all
-    erb( :index)
+    erb( :"animals/index")
 end
 
 get '/animals/new' do
     @vets=Vet.all
-    erb(:animal_registration)
+    erb(:"animals/new")
 end
 
 get '/animals/:id' do #show
     @animal = Animal.find_by_id(params[:id])
-    erb( :show )
+    erb( :"animals/show" )
 end
 
 get '/animals/:id/edit' do
     @vets = Vet.all
     @animal = Animal.find_by_id(params['id'])
-    erb(:edit)
+    erb(:"animals/edit")
 end
 
 post '/animals/:id' do
@@ -46,7 +34,7 @@ end
 post '/animals' do
     @animal = Animal.new( params )
     @animal.save()
-    erb( :registered )
+    erb( :"animals/create" )
 end
 
 post '/animals/:id/delete' do
