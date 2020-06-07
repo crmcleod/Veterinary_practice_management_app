7b0cbe3b880b9d5646f7d8ad2009e4b44b1cfd30
Craig McLeod <mcleod.craig@outlook.com>
2020-06-07 08:37:24 +0100

    Refactored to take out admin and consultation views

--- controllers/admin_controller.rb
+++
@@ -1,9 +0,0 @@
-require('sinatra')
-require('sinatra/contrib/all')
-require_relative('../models/animal')
-require_relative('../models/vet')
-also_reload('../models/*')
-
-get '/admin' do
-    erb(:"admin/index")
-end
--- controllers/consultation_controller.rb
+++
@@ -1,9 +0,0 @@
-require('sinatra')
-require('sinatra/contrib/all')
-require_relative('../models/animal')
-require_relative('../models/vet')
-also_reload('../models/*')
-
-get '/consultation' do
-    erb (:"consultation/index")
-end
