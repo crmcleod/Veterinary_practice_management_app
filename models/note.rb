4ba30bdc882eb98118204d4b3f502600af8fad66
Craig McLeod <mcleod.craig@outlook.com>
2020-06-08 10:17:28 +0100

    Initialize function defined for Not class

--- models/note.rb
+++ models/note.rb
@@ -6,4 +6,11 @@ class Note
     attr_reader :id, :animal_id
     attr_accessor :treatment_notes
 
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @treatment_notes = options['treatment_notes']
+        @animal_id = options['animal_id'].to_i
+    end
+
+    
 end
