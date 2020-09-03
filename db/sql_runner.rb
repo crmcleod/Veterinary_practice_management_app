934f6018656ae7a0af626c41089b6fa0a267f1e4
Craig McLeod <mcleod.craig@outlook.com>
2020-09-03 16:28:17 +0100

    Set up for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -7,8 +7,7 @@ class SqlRunner
       db = PG.connect({dbname: 'dcgdn7lvd3f1ra', host: 'ec2-54-216-202-161.eu-west-1.compute.amazonaws.com', port: 5432, user: 'kfwmtfyowheatr',
         password: 'f074a22a16d59a7941b776e8bcb0b58501a5e29d69ebc396c9ef0abb662c56cc'
         })
-      db.prepare("query", sql)
-      result = db.exec_prepared("query", values)
+      result = db.exec(sql)
     ensure
       db.close() if db != nil
     end
