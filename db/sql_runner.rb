f2b17860a6eee8332f7f5bd75aa576f79feb2dfa
Craig McLeod <mcleod.craig@outlook.com>
2020-09-03 16:41:25 +0100

    Change db info

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,8 +4,11 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'dcgdn7lvd3f1ra', host: 'ec2-54-216-202-161.eu-west-1.compute.amazonaws.com', port: 5432, user: 'kfwmtfyowheatr',
-        password: 'f074a22a16d59a7941b776e8bcb0b58501a5e29d69ebc396c9ef0abb662c56cc'
+      db = PG.connect({dbname: 'df1b1fpld8s6a7', 
+        host: 'ec2-3-248-4-172.eu-west-1.compute.amazonaws.com', 
+        port: 5432, 
+        user: 'onbdnrjflwehfx',
+        password: '15aabee68c62ed545d87494caa60f919ae67a9ce34c1942d97cf6713e91dce55'
         })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
