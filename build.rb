02b7ed65c57572a6137223bad94bce2d842b340c
Craig McLeod <mcleod.craig@outlook.com>
2020-09-04 14:12:53 +0100

    Changes for heroku

--- build.rb
+++ build.rb
@@ -1,3 +1,3 @@
-system('dropdb pace_vma')
-system('createdb pace_vma')
-system('psql -d pace_vma -f ./db/pace.sql && ruby ./db/seeds.rb')
+# system('dropdb pace_vma')
+# system('createdb pace_vma')
+# system('psql -d pace_vma -f ./db/pace.sql && ruby ./db/seeds.rb')
